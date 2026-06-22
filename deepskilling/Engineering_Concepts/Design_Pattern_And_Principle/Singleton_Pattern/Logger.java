f908baa5df3c7091042144bb7e9fc43572236013
Kusumkar-Deeepak <deeepak.kusumkar@gmail.com>
2026-06-23 01:55:52 +0530

    Singleton Pattern Done

---
+++ deepskilling/Engineering_Concepts/Design_Pattern_And_Principle/Singleton_Pattern/Logger.java
@@ -0,0 +1,28 @@
+class Logger
+{
+    // Create only one object
+    private static Logger instance;
+
+    // Private constructor
+    private Logger()
+    {
+        System.out.println("Logger Instance Created");
+    }
+
+    // Public method to access object
+    public static Logger getInstance()
+    {
+        if(instance == null)
+        {
+            instance = new Logger();
+        }
+
+        return instance;
+    }
+
+    // Logging method
+    public void log(String message)
+    {
+        System.out.println("Log: " + message);
+    }
+}
